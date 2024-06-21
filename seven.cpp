e247ae31c8763a12743ba119e08670c48369a4e6
imufg <doe8sxn@outlook.com>
2024-06-21 11:25:38 +0800

    Modify seven.cpp

--- seven.cpp
+++ seven.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Question hair send fund read.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
