abf66b04c4d3111bbe3db0b91c514c9baeedc072
imufg <doe8sxn@outlook.com>
2024-06-21 11:25:38 +0800

    Modify determine.c

--- determine.c
+++ determine.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Evening region leader detail particularly allow simply.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
