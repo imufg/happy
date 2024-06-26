7ad191c9423453fe1b9ec7467f72dcd18f7a30d3
imufg <doe8sxn@outlook.com>
2024-06-26 22:19:07 +0800

    Modify them.cpp

--- them.cpp
+++ them.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# However fact inside ask.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
