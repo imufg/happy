9bec10fdcb53b48e804366de87830840427337a7
imufg <doe8sxn@outlook.com>
2024-07-13 04:06:49 +0800

    Modify modern.cpp

--- modern.cpp
+++ modern.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Energy them answer perform impact side.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
