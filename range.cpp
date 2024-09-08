85e84d2161aa2590575916a13b0a9e3464d7f805
imufg <doe8sxn@outlook.com>
2024-09-08 13:59:25 +0800

    Modify range.cpp

--- range.cpp
+++ range.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Improve different let clear data cover.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
