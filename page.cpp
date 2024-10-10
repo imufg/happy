a8fb4b2ac35ebe40590309d4e3afb9702d72a99a
imufg <doe8sxn@outlook.com>
2024-10-10 13:08:37 +0800

    Modify page.cpp

--- page.cpp
+++ page.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Build anything major time house group.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
