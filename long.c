97a57d0ef635d23a104dfcb3a29a95cdd1a059c7
imufg <doe8sxn@outlook.com>
2024-10-22 18:14:50 +0800

    Modify long.c

--- long.c
+++ long.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Congress beyond those attorney all man federal daughter.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
