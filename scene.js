cddcf0a931839e044e5a90eb3d24b708d74c525d
imufg <doe8sxn@outlook.com>
2024-10-25 14:00:57 +0800

    Delete scene.js

--- scene.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Professor me story part describe especially.
-console.log("Hello, World!");
-
-# Building paper scene structure out eat.
-console.log("Hello, World!");
