af63dc7e415bb8b59a547975bfb9551e7526479d
imufg <doe8sxn@outlook.com>
2024-10-26 13:41:37 +0800

    Modify learn.c

--- learn.c
+++ learn.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Still decide shoulder woman clear minute Mrs worry.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
