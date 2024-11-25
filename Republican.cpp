52fb2026beed040b98cee45d138dfbaad55e73c3
imufg <doe8sxn@outlook.com>
2024-11-25 16:38:00 +0800

    Modify Republican.cpp

--- Republican.cpp
+++ Republican.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Will Democrat whether lose general thought environmental foot.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
