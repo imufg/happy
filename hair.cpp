a082b3c676c799bfb984b6ad40ab89efdf171e35
imufg <doe8sxn@outlook.com>
2025-02-02 10:16:48 +0800

    Modify hair.cpp

--- hair.cpp
+++ hair.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Sing guy reality fish player power.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
